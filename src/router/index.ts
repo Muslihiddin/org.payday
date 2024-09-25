2220eedcd199c331048e59c985f9fc37d1f62ab3
Muslihiddin <muslihiddin97@gmail.com>
2024-09-26 00:02:47 +0500

    fix: scroll position to top on a new page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { routes } from "./routes";
-import { clearLoginToken } from "@/lib/utils";
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from './routes'
+import { clearLoginToken } from '@/lib/utils'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0 }
+  }
+})
 
 const userShouldLogin = (): boolean => {
-  const isTokenAvailable = localStorage.getItem("token");
+  const isTokenAvailable = localStorage.getItem('token')
   if (isTokenAvailable) {
-    const expire = localStorage.getItem("tokenExpire");
+    const expire = localStorage.getItem('tokenExpire')
     if (expire) {
-      const expireTime = new Date(expire);
-      const now = new Date();
-      return expireTime < now;
+      const expireTime = new Date(expire)
+      const now = new Date()
+      return expireTime < now
     } else {
-      return true;
+      return true
     }
   } else {
-    return true;
+    return true
   }
-};
+}
 
 router.beforeEach((to) => {
-  if (to.name === "register" || to.name === "auth-otp-confirmation") {
-    clearLoginToken();
-    return true;
-  } else if (to.name !== "login" && userShouldLogin()) {
-    clearLoginToken();
-    return { name: "login" };
+  if (to.name === 'register' || to.name === 'auth-otp-confirmation') {
+    clearLoginToken()
+    return true
+  } else if (to.name !== 'login' && userShouldLogin()) {
+    clearLoginToken()
+    return { name: 'login' }
   } else {
-    return true;
+    return true
   }
-});
+})
 
-export default router;
+export default router
